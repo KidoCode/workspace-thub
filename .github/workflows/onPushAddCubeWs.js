541998261fd896d79ea564712f808e16fa7ac5f2
Nasrin Tohidi <nasrin@kidocode.com>
2019-12-07 13:49:31 +0800

    Update onPushAddCubeWs.js

--- .github/workflows/onPushAddCubeWs.js
+++ .github/workflows/onPushAddCubeWs.js
@@ -7,8 +7,8 @@ function stop(e) {
 }
 
 const run = async () => {
-    const cube = fs.readFileSync(`./cubes/now`, 'utf8');
-    const userInfo = JSON.parse(fs.readFileSync(`./.cubie/cube.json`, 'utf8')).user;
+    const cube = fs.readFileSync(`cubes/now`, 'utf8');
+    const userInfo = JSON.parse(fs.readFileSync(`.cubie/cube.json`, 'utf8')).user;
     console.log("cube name: ", cube);
     console.log("username: ", userInfo.username);
     try {
